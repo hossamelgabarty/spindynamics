04a9587a7216a4153caad357b9bcd65e3b7d7a6d
hossamelgabarty <hossam.elgabarty@fu-berlin.de>
2013-03-22 14:20:37 +0100

    cmake install

--- mpi/mpi_master.cpp
+++ mpi/mpi_master.cpp
@@ -115,7 +115,7 @@ void MPIMaster::calculateIntensity(const fp from, const fp to, const int steps, 
     cout << timeStamp() << "continuing with " << m_jobQueue.size() << " jobs from last run" << endl;
   }
 
-  m_intensityOutputFile = m_outputDir + input_filename + "_spectrum.at";
+  m_intensityOutputFile = m_outputDir + "/" + input_filename + "_spectrum.dat";
   // truncate data file if we start from scratch
   m_intensityOutput.open(m_intensityOutputFile.data(), ios_base::out | (continuingJobs ? ios_base::app : ios_base::trunc));
   if (!m_intensityOutput.is_open()) {
